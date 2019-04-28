e3d883daa0cf4211367af918ff42336877dfb4d0
Mior Muhammad Zaki <crynobone@gmail.com>
2019-04-28 13:45:31 +0800

    Add docblock.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Katsana.php
+++ src/Katsana.php
@@ -4,6 +4,15 @@ namespace Katsana;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method mixed config(string|null $key = null)
+ * @method \Katsana\Sdk\Client driver(string|null $driver = null)
+ * @method \Laravie\Codex\Contracts\Request via(\Laravie\Codex\Contracts\Request $request)
+ * @method \Katsana\Sdk\Client onTimeZone(string $timeZoneCode)
+ * @method \Katsana\Sdk\Client useCustomApiEndpoint(string $endpoint)
+ *
+ * @see \Katsana\Manager
+ */
 class Katsana extends Facade
 {
     /**
