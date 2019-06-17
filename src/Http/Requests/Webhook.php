a78f95e3114d507a8d46a52e0723b706aaafaf47
Mior Muhammad Zaki <crynobone@gmail.com>
2019-06-18 06:24:25 +0800

    Cache client manager value.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Http/Requests/Webhook.php
+++ src/Http/Requests/Webhook.php
@@ -2,11 +2,19 @@
 
 namespace Katsana\Http\Requests;
 
+use Katsana\Manager;
 use Katsana\Sdk\Signature;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Webhook extends Request
 {
+    /**
+     * The SDK Client Manager.
+     *
+     * @var \Katsana\Manager
+     */
+    protected $sdk;
+
     /**
      * The request signature key.
      *
@@ -34,12 +42,12 @@ class Webhook extends Request
      */
     public function validated(): array
     {
-        $config = $this->container->make('katsana.manager')->config('webhook');
-
-        $signature = new Signature($config['signature']);
+        $signature = new Signature($this->getSignatureKey());
         $header = $this->header('X_SIGNATURE') ?? '';
 
-        if (! $signature->verify($header, $this->getContent(), ($config['threshold'] ?? 3600))) {
+        $threshold = $this->getClientManager()->config('webhook.threshold') ?? 3600;
+
+        if (! $signature->verify($header, $this->getContent(), $threshold)) {
             throw new HttpException(419, 'Unable to verify X-Signature.');
         }
 
@@ -53,7 +61,7 @@ class Webhook extends Request
      */
     protected function getSignatureKey(): ?string
     {
-        return $this->signatureKey ?? $this->container->make('katsana.manager')->config('webhook');
+        return $this->signatureKey ?? $this->getClientManager()->config('webhook.signature');
     }
 
     /**
@@ -68,5 +76,19 @@ class Webhook extends Request
         $this->signatureKey = $signatureKey;
 
         return $this;
+    }
+
+    /**
+     * Get SDK Client Manager.
+     *
+     * @return \Katsana\Manager
+     */
+    final public function getClientManager(): Manager
+    {
+        if (! isset($this->sdk)) {
+            $this->sdk = $this->container->make('katsana.manager');
+        }
+
+        return $this->sdk;
     }
 }
